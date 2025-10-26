5eceb1c2b85f1b41d8f4cab48cadc95fd0bd4b65
Shardulkacheria <141412151+Shardulkacheria@users.noreply.github.com>
2025-10-26 18:30:44 +0530

    added fireabse using env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBR_laBiS7VjK7ADJL8SePRtqBQCflCNy0",
-  authDomain: "rezzai-4dcd5.firebaseapp.com",
-  projectId: "rezzai-4dcd5",
-  storageBucket: "rezzai-4dcd5.firebasestorage.app",
-  messagingSenderId: "74146917631",
-  appId: "1:74146917631:web:c5f2e640504eebb3a670ca",
-  measurementId: "G-7VB29BGTLF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
